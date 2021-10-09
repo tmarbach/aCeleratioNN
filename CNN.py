591d79f7c3f02be58beebb47e3a527d93bc998bb
Tyler <tymarbach@gmail.com>
2021-10-08 23:16:59 -0700

    code commit

---
+++ CNN.py
@@ -0,0 +1,6 @@
+#Dont forget to be in the tf conda env before starting!
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+
