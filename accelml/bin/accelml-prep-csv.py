3e59ceca1ed7210070c6c423090f5f25236260b8
Tyler <tymarbach@gmail.com>
2021-10-18 13:21:30 -0700

    better formatting and changed input file type
    Input file type is no longer opened and formatting has been improved slightly

--- accelml/bin/accelml-prep-csv.py
+++ accelml/bin/accelml-prep-csv.py
@@ -11,13 +11,15 @@ import pandas as pd
 import argparse
 
 def arguments():
-    parser = argparse.ArgumentParser(prog='accelml_prep_csv',
-      description="Clean and prepare accelerometer csv data for CNN input by rounding to 3 decimal places\
-       and removing blank timestamps",\
-        epilog="Columns of accelerometer data must be arranged:'tag_id', 'date', 'time', 'camera_date', 'camera_time', \
+    parser = argparse.ArgumentParser(prog='accelml_prep_csv', 
+            description="Clean and prepare accelerometer csv data for CNN input by rounding to\
+                         3 decimal places and removing blank timestamps",
+            epilog="Columns of accelerometer data must be arranged:'tag_id', 'date', 'time', 'camera_date', 'camera_time', \
                   'behavior', 'acc_x', 'acc_y', 'acc_z', 'temp_c', 'battery_voltage', 'metadata'")
-    parser.add_argument("csv_file", type=argparse.FileType('r'), help = "input the path to the csv file of accelerometer data that\
-                                       requires cleaning")
+    parser.add_argument(
+            "csv_file",
+            type=str,
+            help = "input the path to the csv file of accelerometer data that requires cleaning")
     parser.add_argument("-o", "--output", help="Directs the output to a name of your choice")
     return parser.parse_args()
 
