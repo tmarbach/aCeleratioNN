0b67c5c110bd55913f3f2df946f8e65d3c78cf83
Tyler <tymarbach@gmail.com>
2021-11-01 13:39:40 -0700

    adding average times script and packaging notebook
    new script is testing the generation of an average time per class, still needs a lot of work
    notebook is a test for breaking the data into the chunks that the above script will determine
    the size of.

---
+++ accelml/bin/averagetimes.py
@@ -0,0 +1,51 @@
+from os import error
+import pandas as pd
+
+
+# I want this script to read a csv of one behavior
+    # pull unique times from it
+    # determine the chunks of time (consecutive)
+    # average the length of time of the behavior (sum(time between start & end)/# of chunks)
+    # that number * 25 should be the window size for the CNN
+
+def average_generator(single_behavior_csv):
+    #when taking in the correct csvs, header will exist already
+    rdf = pd.read_csv(single_behavior_csv,low_memory=False, names=['origin_file',
+                                                                    'other_origin',
+                                                                    'date',
+                                                                    'time',
+                                                                    'cam-date',
+                                                                    'cam-time',
+                                                                    'behavior',
+                                                                    'acc_x',
+                                                                    'acc_y',
+                                                                    'acc_z',
+                                                                    'temp_c',
+                                                                    'battery_voltage',
+                                                                    'metadata'])    
+    
+    df = rdf[rdf.date != '0']
+
+# Just gonna combine the 'date' and 'time' columns = datetime (this is easier to parse)
+    df['date']= df['date'].str.replace('/','-')
+
+    df['date_time'] = pd.to_datetime(df['date'] + ' ' + df['time'],
+                                     format = '%d-%m-%Y %H:%M:%S')
+    uniquedf = df.date_time.unique()
+# once coverted to datetime, take unique times (nparray)
+# then make new df with just the unique times and use below code to determine chunks
+# eventually, output data to a file. Not perfect, but can get us started. 
+#DOESNT WORK IF DATA ISNT CLEAN aka time or date is bad. 
+#Now in date_time format, code below can chunk it into groups
+    dt = pd.DataFrame(uniquedf, columns = ['date_time'])
+    sec = pd.Timedelta('1sec')
+    breaks = dt.date_time.diff() != sec
+    groups = breaks.cumsum()
+    for group in dt.groupby(groups):
+        print(group[1])
+        #currently, prints tuple objects where the second element provides the number of seconds
+        # no idea how to get access to that number currently.
+
+
+if __name__ == "__main__":
+    average_generator('../MLSnakes/test_data/combined-training/allingest.csv')
