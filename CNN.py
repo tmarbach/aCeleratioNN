7397d1f8265839026685004c2c58f45678b8b315
Tyler <tymarbach@gmail.com>
2021-11-02 11:19:13 -0700

    Removed data and old notebooks/scripts
    removed old notebooks and scripts from the repo to clean it up
    i should not have had the data there in the first place so Im fixing it.
    The scripts and notebooks are either old test scripts or have been incorporated
    into the main script already.

--- CNN.py
+++
@@ -1,6 +0,0 @@
-#Dont forget to be in the tf conda env before starting!
-import tensorflow as tf
-import numpy as np
-from tensorflow.keras import datasets, layers, models
-import matplotlib.pyplot as plt
-
