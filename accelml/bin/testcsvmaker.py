b2f6067535afe6adb137bec76606224288f4ae67
Tyler <tymarbach@gmail.com>
2021-11-04 11:51:29 -0700

    testcsvmaker takes og data not clean data
    now creates a test csv using the original data, not the cleaned reorganized data
    this should make for more realistic tests moving forward.

--- accelml/bin/testcsvmaker.py
+++ accelml/bin/testcsvmaker.py
@@ -4,9 +4,9 @@ def test_csv_generator(cleancsv):
     df = pd.read_csv(cleancsv)
     emptydf = pd.DataFrame(columns = df.columns)
 
-    behaviors = df.behavior.unique()
+    behaviors = df.Behavior.unique()
     for behavior in behaviors:
-        bdf = df.loc[df['behavior'] == behavior]
+        bdf = df.loc[df['Behavior'] == behavior]
         emptydf = emptydf.append(bdf.head(15))
 
     return emptydf
@@ -17,8 +17,8 @@ def test_df_writer(testdf, output_location):
 
 
 def main():
-    testr = test_csv_generator("../../../CNNworkspace/cleanPenn.csv")
-    test_df_writer(testr, "../../../CNNworkspace/testdata.csv")
+    testr = test_csv_generator("../../../MLSnakes/Copy_of_Pen11_Full_CSV_Final_Round_1.csv")
+    test_df_writer(testr, "../../../CNNworkspace/realtestdata.csv")
 
 if __name__=="__main__":
     main()
