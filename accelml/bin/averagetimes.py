f807b0f4cf4b5f004db0eb6ad1bf599107da81b4
Tyler <tymarbach@gmail.com>
2021-11-04 12:13:51 -0700

    averagetimes takes clean data now
    now takes cleaned data, for each behavior it finds the average time for the number of
    instances a behavior occurs (test data will be one instance for each) this will be used
    to determine the window used in the csv

--- accelml/bin/averagetimes.py
+++ accelml/bin/averagetimes.py
@@ -2,44 +2,36 @@ from os import error
 import pandas as pd
 
 
-# I want this script to read a csv of one behavior
+# I want this script to read a csv of cleaned data
     # pull unique times from it
     # determine the chunks of time (consecutive)
     # average the length of time of the behavior (sum(time between start & end)/# of chunks)
     # that number * 25 should be the window size for the CNN
 
-def average_generator(single_behavior_csv):
-    #when taking in the correct csvs, header will exist already
-    rdf = pd.read_csv(single_behavior_csv,low_memory=False, names=['origin_file',
-                                                                    'other_origin',
-                                                                    'date',
-                                                                    'time',
-                                                                    'cam-date',
-                                                                    'cam-time',
-                                                                    'behavior',
-                                                                    'acc_x',
-                                                                    'acc_y',
-                                                                    'acc_z',
-                                                                    'temp_c',
-                                                                    'battery_voltage',
-                                                                    'metadata'])    
-    
-    df = rdf[rdf.date != '0']
+def class_divider(cleancsv):
+    """
+    Args:
+    cleanedcsv -- output of accelml-prep-csv, csv file of clean 
+                        annotated and timestamped accelerometer data.     
+    Returns:
+    stat_dict -- dict of behavior code and the stats of # of instances
+                    and length of instances EX: {'t':[4,1], 'i':[3,500]}   
+    """
+    stat_dict = {}
+    df = pd.read_csv(cleancsv)
+    behaviors = df.behavior.unique()
+    for behavior in behaviors:
+        bdf = df.loc[df['behavior'] == behavior]# df with one behavior
+        bdf = bdf.columns = df.columns
+        stats = average_generator(bdf)
+        stat_dict[behavior].append(stats)
+         
+    return stat_dict
 
-# Just gonna combine the 'date' and 'time' columns = datetime (this is easier to parse)
-    df['date']= df['date'].str.replace('/','-')
 
-    df['date_time'] = pd.to_datetime(df['date'] + ' ' + df['time'],
-                                     format = '%d-%m-%Y %H:%M:%S')
+def average_generator(one_class_df):
     uniquedf = df.date_time.unique()
-# once coverted to datetime, take unique times (nparray)
-# then make new df with just the unique times and use below code to determine chunks
-# eventually, output data to a file. Not perfect, but can get us started. 
-#DOESNT WORK IF DATA ISNT CLEAN aka time or date is bad. 
-#Now in date_time format, code below can chunk it into groups
     dt = pd.DataFrame(uniquedf, columns = ['date_time'])
-
-
     sec = pd.Timedelta('1sec')
     breaks = dt.date_time.diff() != sec
     groups = breaks.cumsum()
